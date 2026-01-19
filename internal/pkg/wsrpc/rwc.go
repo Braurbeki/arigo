a5620ea17cc6178e140f762c473689c7612685ca
aolkhovs <artem.olkhovskyi@avid.com>
2026-01-19 18:43:18 +0200

    Websocket panic fix

--- internal/pkg/wsrpc/rwc.go
+++ internal/pkg/wsrpc/rwc.go
@@ -3,12 +3,15 @@
 package wsrpc
 
 import (
-	"github.com/gorilla/websocket"
 	"io"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 // ReadWriteCloser is a rwc based on WebSockets
 type ReadWriteCloser struct {
+	mu sync.Mutex
 	ws *websocket.Conn
 	r  io.Reader
 	w  io.WriteCloser
@@ -21,20 +24,43 @@ func NewReadWriteCloser(ws *websocket.Conn) ReadWriteCloser {
 
 // Read reads from the WebSocket into p
 func (rwc *ReadWriteCloser) Read(p []byte) (n int, err error) {
-	if rwc.r == nil {
-		_, rwc.r, err = rwc.ws.NextReader()
+	var r io.Reader
+	var ws *websocket.Conn
+
+	rwc.mu.Lock()
+	ws = rwc.ws
+	r = rwc.r
+	rwc.mu.Unlock()
+
+	if ws == nil {
+		return 0, io.ErrClosedPipe
+	}
+
+	if r == nil {
+		_, r, err = ws.NextReader()
 		if err != nil {
 			return 0, err
 		}
+		rwc.mu.Lock()
+		if rwc.ws == nil {
+			rwc.mu.Unlock()
+			return 0, io.ErrClosedPipe
+		}
+		rwc.r = r
+		rwc.mu.Unlock()
 	}
 
 	for n = 0; n < len(p); {
 		var m int
-		m, err = rwc.r.Read(p[n:])
+		m, err = r.Read(p[n:])
 		n += m
 		if err == io.EOF {
 			// done
-			rwc.r = nil
+			rwc.mu.Lock()
+			if rwc.r == r {
+				rwc.r = nil
+			}
+			rwc.mu.Unlock()
 			break
 		}
 		if err != nil {
@@ -47,23 +73,50 @@ func (rwc *ReadWriteCloser) Read(p []byte) (n int, err error) {
 
 // Write writes the provided bytes to the WebSocket
 func (rwc *ReadWriteCloser) Write(p []byte) (n int, err error) {
-	if rwc.w == nil {
-		rwc.w, err = rwc.ws.NextWriter(websocket.TextMessage)
+	var w io.WriteCloser
+	var ws *websocket.Conn
+
+	rwc.mu.Lock()
+	ws = rwc.ws
+	w = rwc.w
+	rwc.mu.Unlock()
+
+	if ws == nil {
+		return 0, io.ErrClosedPipe
+	}
+
+	if w == nil {
+		w, err = ws.NextWriter(websocket.TextMessage)
 		if err != nil {
 			return 0, err
 		}
+		rwc.mu.Lock()
+		if rwc.ws == nil {
+			rwc.mu.Unlock()
+			_ = w.Close()
+			return 0, io.ErrClosedPipe
+		}
+		rwc.w = w
+		rwc.mu.Unlock()
 	}
 
 	for n = 0; n < len(p); {
-		m, err := rwc.w.Write(p)
+		m, err := w.Write(p[n:])
 		n += m
 		if err != nil {
 			break
 		}
 	}
 	if err != nil || n == len(p) {
-		err = rwc.w.Close()
-		rwc.w = nil
+		closeErr := w.Close()
+		rwc.mu.Lock()
+		if rwc.w == w {
+			rwc.w = nil
+		}
+		rwc.mu.Unlock()
+		if err == nil {
+			err = closeErr
+		}
 	}
 
 	return
@@ -72,15 +125,24 @@ func (rwc *ReadWriteCloser) Write(p []byte) (n int, err error) {
 // Close the rwc and the underlying WebSocket connection
 func (rwc *ReadWriteCloser) Close() error {
 	var err error
-	if rwc.w != nil {
-		if err = rwc.w.Close(); err != nil {
+	var w io.WriteCloser
+	var ws *websocket.Conn
+
+	rwc.mu.Lock()
+	w = rwc.w
+	rwc.w = nil
+	rwc.r = nil
+	ws = rwc.ws
+	rwc.ws = nil
+	rwc.mu.Unlock()
+
+	if w != nil {
+		if err = w.Close(); err != nil {
 			return err
 		}
-		rwc.w = nil
 	}
-	if rwc.ws != nil {
-		err = rwc.ws.Close()
-		rwc.ws = nil
+	if ws != nil {
+		return ws.Close()
 	}
-	return err
+	return nil
 }
